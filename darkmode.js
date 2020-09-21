957fa466f56c2ce89c047e00d31fbd69199b24c4
njpatt <55141626+njpatt@users.noreply.github.com>
2020-09-21 14:10:26 -0400

    Finished Uploading rest of files

---
+++ darkmode.js
@@ -0,0 +1,18 @@
+import Darkmode from 'darkmode-js';
+
+var options = {
+    bottom: '64px', // default: '32px'
+    right: 'unset', // default: '32px'
+    left: '32px', // default: 'unset'
+    time: '0.2s', // default: '0.3s'
+    mixColor: '#00b386', // default: '#fff'
+    backgroundColor: '#fff',  // default: '#fff'
+    buttonColorDark: '#100f2c',  // default: '#100f2c'
+    buttonColorLight: '#fff', // default: '#fff'
+    saveInCookies: false, // default: true,
+    label: '🌓', // default: ''
+    autoMatchOsTheme: true // default: true
+  }
+  
+  const darkmode = new Darkmode(options);
+  darkmode.showWidget();
